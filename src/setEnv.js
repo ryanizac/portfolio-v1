6d25aaaa8c758f8af07d961ec91585a49c2eac6b
Ryan <io.ryan@outlook.com>
2022-01-11 10:37:17 -0300

    removed nodejs application

--- src/setEnv.js
+++
@@ -1,8 +0,0 @@
-const dotenv = require('dotenv');
-const path = require('path');
-
-const envs = dotenv.config({
-    path: path.resolve(__dirname, '../.env')
-});
-
-module.exports = envs.error || true;
